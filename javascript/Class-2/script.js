a672ff95524a5c6442c0070d8120d7b5128b1d2a
Javkhlant <enrosek95@gmail.com>
2022-11-29 15:58:40 +0800

    Class

---
+++ javascript/Class-2/script.js
@@ -0,0 +1,39 @@
+// let age = 17;
+
+// if (age < 0) {
+//   console.log("Буруу нас байна!");
+// } else if (age >= 2 && age <= 5) {
+//   console.log("Цэцэрлэгийн хүүхэд!");
+// } else if (age >= 6 && age <= 18) {
+//   console.log("10 жилийн хүүхэд байна!");
+// } else {
+//   console.log("Том хүн байна!");
+// }
+
+// let bataaAge = 15,
+//   boldooAge = 12;
+
+// if (bataaAge > boldooAge) console.log("Батаа ах!");
+// else if (bataaAge === boldooAge) console.log("Чацуу байна!");
+// else console.log("Болдоо ах!");
+
+// let a = 10,
+//   b = 12,
+//   c = 12;
+
+// if (a >= b && a >= c) console.log(a);
+// else if (b >= a && b >= c) console.log(b);
+// else if (c >= a && c >= b) console.log(c);
+
+// let num = 19;
+
+// if (num % 2 === 0) console.log("Тэгш");
+// else console.log("Сондгой");
+
+const year = 2022;
+
+if ((year % 4 === 0 && year % 100 !== 0) || year % 400 === 0) {
+  console.log("Leap Year");
+} else {
+  console.log("Not leap year");
+}
