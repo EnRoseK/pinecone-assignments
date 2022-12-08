c7bf2427ffd8fd9cfd531c2278c637c35cae02fc
Javkhlant <enrosek95@gmail.com>
2022-12-08 18:09:01 +0800

    Simple

---
+++ javascript/js-object/simple-object/script.js
@@ -0,0 +1,12 @@
+const dog = {};
+
+console.log(dog);
+
+dog.breed = 'Toy Terrier';
+dog.name = 'Toxy';
+dog.color = 'Golden';
+dog.lovable = 'Cranky AF';
+
+console.log(dog);
+
+console.log(Object.values(dog));
