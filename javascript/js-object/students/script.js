c8c5112dfc3ba5ff70b77f43c0cc52a1a55d5604
Javkhlant <enrosek95@gmail.com>
2022-12-08 16:53:57 +0800

    Students

---
+++ javascript/js-object/students/script.js
@@ -0,0 +1,40 @@
+const students = [
+    { name: 'Гончигсумлай', birthOfYear: 1995, hobby: 'морь', single: false },
+    { name: 'Сэд-Эрдэнэ', birthOfYear: 2005, hobby: 'тоглох', single: true },
+    { name: 'Хатнаа', birthOfYear: 2002, hobby: 'аялах', single: false },
+    { name: 'Индра', birthOfYear: 1997, hobby: 'ууланд алхах', single: true },
+    { name: 'Биндэрьяа', birthOfYear: 1998, hobby: 'унтах', single: false },
+    { name: 'Жавхлант', birthOfYear: 1999, hobby: 'спорт', single: true },
+    { name: 'Тэмүүлэн', birthOfYear: 1993, hobby: 'спорт', single: false },
+];
+
+const getStudentAge = (student) => {
+    return 2022 - student.birthOfYear;
+};
+
+const getStudentsOlderThanTwenty = (students) => {
+    const result = [];
+    for (let i = 0; i < students.length; i++) {
+        if (getStudentAge(students[i]) >= 20) result.push(students[i]);
+    }
+
+    return result;
+};
+
+const getSingleStudents = (students) => {
+    const result = [];
+    for (let i = 0; i < students.length; i++) {
+        if (students[i].single) result.push(students[i]);
+    }
+
+    return result;
+};
+
+const firstStudentAge = getStudentAge(students[0]);
+console.log(firstStudentAge);
+
+const studentsOlderThanTwenty = getStudentsOlderThanTwenty(students);
+console.log(studentsOlderThanTwenty);
+
+const singleStudents = getSingleStudents(students);
+console.log(singleStudents);
