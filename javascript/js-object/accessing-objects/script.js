4a031bf68191c11b5304a9e1fad042a8ac4a54ea
Javkhlant <enrosek95@gmail.com>
2022-12-08 16:41:42 +0800

    Class

---
+++ javascript/js-object/accessing-objects/script.js
@@ -0,0 +1,82 @@
+const students = [
+    {
+        name: 'Сэд-Эрдэнэ',
+        age: 19,
+        gender: 'male',
+    },
+    {
+        name: 'Индра',
+        age: 19,
+        gender: 'female',
+    },
+    {
+        name: 'Хатнаа ',
+        age: 21,
+        gender: 'male',
+    },
+    {
+        name: 'Тэмүүлэн',
+        age: 24,
+        gender: 'male',
+    },
+    {
+        name: 'Намуун',
+        age: 23,
+        gender: 'female',
+    },
+];
+
+const getAgeAverage = (arr) => {
+    let sum = 0;
+    for (let i = 0; i < arr.length; i++) {
+        sum += arr[i].age;
+    }
+
+    return sum / arr.length;
+};
+
+const ageAverage = getAgeAverage(students);
+console.log(ageAverage);
+
+const getGenderCount = (arr) => {
+    const counts = { male: 0, female: 0 };
+    for (let i = 0; i < arr.length; i++) {
+        arr[i].gender.toLowerCase() === 'male' ? counts.male++ : counts.female++;
+    }
+
+    return counts;
+};
+
+const genderCounts = getGenderCount(students);
+console.log(genderCounts);
+
+const addLastName = (student, lastName) => {
+    student.lastName = lastName;
+};
+
+addLastName(students[0], 'Одгэрэл');
+console.log(students[0]);
+
+const getStudentsWithSameAge = (arr) => {
+    let searchValue;
+    const result = [];
+    for (let i = 0; i < arr.length; i++) {
+        let count = 0;
+        for (let j = 0; j < arr.length; j++) {
+            if (arr[i].age === arr[j].age) count++;
+        }
+        if (count >= 2) {
+            searchValue = arr[i].age;
+            break;
+        }
+    }
+
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i].age === searchValue) result.push(arr[i]);
+    }
+
+    return result;
+};
+
+const studentsWithSameAge = getStudentsWithSameAge(students);
+console.log(studentsWithSameAge);
---
+++ javascript/js-object/class/script.js
@@ -0,0 +1,76 @@
+const student = {
+    firstName: 'Sed-Erdene',
+    lastName: 'Odgerel',
+    age: 17,
+    hobby: ['Video Games', 'Watching Streams', 'Anime', 'Panda'],
+};
+
+// console.log(`${student.lastName} овогтой ${student.firstName} нэртэй ${student.age} настай`);
+
+const studentCopy = { ...student };
+studentCopy.age = 23;
+// console.log(student['age']);
+// console.log(studentCopy.age);
+
+const data = [
+    { gender: 'male', firstName: 'Dorj', lastName: 'Bat', point: 80, age: 19 },
+    { gender: 'female', firstName: 'Nawchaa', lastName: 'Gonchigsumlai', point: 68, age: 99 },
+    { gender: 'male', firstName: 'Bold', lastName: 'Balbar', point: 75, age: 29 },
+    { gender: 'female', firstName: 'Tsetsgee', lastName: 'Hatnaa', point: 55, age: 89 },
+    { gender: 'male', firstName: 'Bat', lastName: 'Sed-Erdene', point: 23, age: 39 },
+    { gender: 'female', firstName: 'Sed-Erdene', lastName: 'Dorj', point: 81, age: 79 },
+    { gender: 'male', firstName: 'Balbar', lastName: 'Gonchigsumlai', point: 44, age: 49 },
+    { gender: 'female', firstName: 'Hatnaa', lastName: 'Saraa', point: 90, age: 69 },
+    { gender: 'male', firstName: 'Gonchigsumlai', lastName: 'Nawchaa', point: 85, age: 59 },
+    { gender: 'female', firstName: 'Saraa', lastName: 'Tsetsgee', point: 99, age: 15 },
+];
+
+const getGenderCount = (data, gender) => {
+    let count = 0;
+    for (let i = 0; i < data.length; i++) {
+        if (data[i].gender === gender.toLowerCase()) count++;
+    }
+
+    return count;
+};
+
+const femaleCount = getGenderCount(data, 'Female'),
+    maleCount = getGenderCount(data, 'Male');
+
+console.log(`Эрэгтэй: ${maleCount}, Эмэгтэй: ${femaleCount}`);
+
+const getCountByFieldValueRange = (data, min, max, key) => {
+    let count = 0;
+    for (let i = 0; i < data.length; i++) {
+        if (data[i][key] >= min && data[i][key] < max) count++;
+    }
+
+    return count;
+};
+
+const childrenCount = getCountByFieldValueRange(data, 0, 18, 'age'),
+    youthCount = getCountByFieldValueRange(data, 18, 30, 'age'),
+    adultCount = getCountByFieldValueRange(data, 30, 55, 'age'),
+    elderCount = getCountByFieldValueRange(data, 55, 200, 'age');
+
+console.log(childrenCount, youthCount, adultCount, elderCount);
+
+const totalA = getCountByFieldValueRange(data, 90, 101, 'point'),
+    totalB = getCountByFieldValueRange(data, 80, 90, 'point'),
+    totalC = getCountByFieldValueRange(data, 70, 80, 'point'),
+    totalD = getCountByFieldValueRange(data, 60, 70, 'point'),
+    totalF = getCountByFieldValueRange(data, 0, 60, 'point');
+
+console.log(totalA, totalB, totalC, totalD, totalF);
+
+const searchStudent = (data, key, str) => {
+    const result = [];
+    for (let i = 0; i < data.length; i++) {
+        if (data[i][key].toLowerCase().includes(str.toLowerCase())) result.push(data[i]);
+    }
+
+    return result;
+};
+
+const search = searchStudent(data, 'firstName', 'hat');
+console.log(search);
