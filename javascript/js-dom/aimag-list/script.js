e5d6e39b76d7141ad9be5adae0204d8dc42edd09
Javkhlant Altankhuyag <enroseking95@gmail.com>
2022-12-14 10:45:00 +0800

    aimag list

--- javascript/js-dom/aimag-list/script.js
+++ javascript/js-dom/aimag-list/script.js
@@ -15,3 +15,26 @@ for (let i = 0; i < liEls.length; i++) {
     liEls[i].style.color = 'brown';
     liEls[i].style.marginTop = '10px';
 }
+
+for (let i = 0; i < liEls.length; i++) {
+    const aimagId = liEls[i].id.split('-');
+    switch (aimagId[1][0]) {
+        case '1':
+            liEls[i].style.background = 'green';
+            break;
+        case '2':
+            liEls[i].style.background = 'red';
+            break;
+        case '3':
+            liEls[i].style.background = 'blue';
+            break;
+        case '4':
+            liEls[i].style.background = 'yellow';
+            break;
+        case '5':
+            liEls[i].style.background = 'aqua';
+            break;
+        default:
+            break;
+    }
+}
