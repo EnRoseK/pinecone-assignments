040d018237497932c1775c980c85111fb5b57939
Javkhlant Altankhuyag <enroseking95@gmail.com>
2022-12-17 22:17:44 +0800

    Some work

--- javascript/js-dom/remove-from-dropdown/script.js
+++ javascript/js-dom/remove-from-dropdown/script.js
@@ -1,3 +1,25 @@
 const colorSelect = document.getElementById('colorSelect');
+const btn = document.querySelector('input[type="button"]');
+const options = document.getElementsByTagName('option');
 
-colorSelect.addEventListener('change', (e) => {});
+const alertElements = () => {
+    let result = `No. of items : ${options.length}\n`;
+
+    for (let i = 0; i < options.length; i++) {
+        result += options[i].value + '\n';
+    }
+
+    alert(result);
+};
+
+btn.addEventListener('click', () => {
+    for (let i = 0; i < options.length; i++) {
+        if (options[i].value === colorSelect.value) {
+            options[i].parentNode.removeChild(options[i]);
+        }
+    }
+
+    alertElements();
+});
+
+alertElements();
