f9a5d8f5102904b771fe1f0df81845b0e83838f8
Javkhlant <enrosek95@gmail.com>
2023-01-03 18:08:21 +0800

    Tried some shit

--- reactjs/projects/admin-panel/src/App.jsx
+++ reactjs/projects/admin-panel/src/App.jsx
@@ -1,11 +1,17 @@
 import './styles/bootstrap.min.css';
 import './styles/styles.css';
 import { Navbar } from './components/Navbar';
+import { Sidebar } from './components/Sidebar';
+import { Dashboard } from './components/Dashboard';
 
 const App = () => {
     return (
         <>
             <Navbar />
+            <main className='d-flex flex-nowrap'>
+                <Sidebar />
+                <Dashboard />
+            </main>
         </>
     );
 };
--- reactjs/projects/admin-panel/src/components/Navbar/NavbarWrapper.jsx
+++ reactjs/projects/admin-panel/src/components/Navbar/NavbarWrapper.jsx
@@ -1,7 +1,7 @@
 export const NavbarWrapper = ({ children }) => {
     return (
         <nav className='navbar navbar-expand-lg navbar-dark bg-dark'>
-            <div className='container'>{children}</div>
+            <div className='container-fluid'>{children}</div>
         </nav>
     );
 };
